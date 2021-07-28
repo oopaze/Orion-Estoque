02ba230a819a2e0782318e7da981ca1208f49de7
José Pedro <jgomes@geobit.com.br>
2021-07-28 00:05:33 -0300

    adicionando base do template

--- app/produto/models.py
+++ app/produto/models.py
@@ -13,7 +13,7 @@ class Produto(models.Model):
     valor_revenda = models.DecimalField(decimal_places=2,max_digits=10)
     quantidade = models.PositiveIntegerField()
     
-    criado_em = models.DateField(auto_created=now)
+    criado_em = models.DateField(auto_now_add=now)
 
     def __str__(self):
         return f"{self.marca} - {self.armazenamento} - {self.modelo} - (R$ {self.valor_revenda})"
