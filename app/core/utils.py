78383303db12bd9479431eea3a29aac890e3d5e8
José Pedro <jgomes@geobit.com.br>
2021-08-20 03:30:54 -0300

    fixing get bairros error

--- app/core/utils.py
+++ app/core/utils.py
@@ -31,19 +31,23 @@ def get_detail_instance(instance, ignore_fields=[], last_fields=[]):
 
 
 def get_bairros_choice(cidade=None):
-    if cidade:
-        with open(f"static/bairros_{cidade}.json", 'r') as f:
-           return [(bairro.replace(" ", "_"), f"{bairro} ({cidade})") for bairro in json.loads(f.read())]
+    try:
+        if cidade:
+            with open(f"static/bairros_{cidade}.json", 'r') as f:
+                return [(bairro.replace(" ", "_"), f"{bairro} ({cidade})") for bairro in json.loads(f.read())]
 
-    else:
-        with open("static/bairros_Crato.json", 'r') as f:
-           bairros_crato = [
-               (bairro.replace(" ", "_"), f"{bairro} (Crato)") for bairro in json.loads(f.read())
-            ]
+        else:
+            with open("static/bairros_Crato.json", 'r') as f:
+                bairros_crato = [
+                    (bairro.replace(" ", "_"), f"{bairro} (Crato)") for bairro in json.loads(f.read())
+                ]
 
-        with open("static/bairros_Juazeiro do Norte.json", 'r') as f:
-           bairros_jn = [
-               (bairro.replace(" ", "_"), f"{bairro} (Juazeiro do Norte)") for bairro in json.loads(f.read())
-            ]
+            with open("static/bairros_Juazeiro do Norte.json", 'r') as f:
+                bairros_jn = [
+                    (bairro.replace(" ", "_"), f"{bairro} (Juazeiro do Norte)") for bairro in json.loads(f.read())
+                ]
 
         return bairros_crato + bairros_jn
+
+    except FileNotFoundError:
+        return (("", "-------"),)
